b4c1fe1dfcdda323a0a3a3b1be5fbcb4ef8638c3
leonardo3108 <leonardo3108@gmail.com>
2021-12-01 23:17:16 -0300

    day 1 - part Two

--- src/Day01.kt
+++ src/Day01.kt
@@ -2,12 +2,25 @@ import java.io.File
 
 fun main() {
     var oldValue = 99999
-    var count = 0
+    var countOne = 0
+    var countThree = 0
+    var last = 99999
+    var penultimate = 99999
+    var oldSigma = 99999
     File("resource/day01.txt").forEachLine {
         val newValue = it.toInt()
+        val sigma = newValue + last + penultimate
         if (newValue > oldValue)
-            count ++
+            countOne ++
+        if ((sigma < 99999) && (sigma > oldSigma))
+            countThree ++
         oldValue = newValue
+        oldSigma = sigma
+        penultimate = last
+        last = newValue
     }
-    print(count)
+    print("Part One:")
+    println(countOne)
+    print("Part Two:")
+    println(countThree)
 }
